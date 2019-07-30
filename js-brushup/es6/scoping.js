20d82da6dfecfcadfb2c50a67f87f93c2f56cc6e
gagan <gaganparmar110@gmail.com>
2019-07-30 23:55:30 +0530

    scoping

---
+++ js-brushup/es6/scoping.js
@@ -0,0 +1,12 @@
+var num=10
+if(true){
+    var num=20
+}
+console.log(`number is `+num)
+console.log(`number is ${num}`)
+console.log(`
+     hyyyyyyyyy
+     helloooooooooo
+     byeeeeeeeeeeeeee
+`)
+console.log(`date is${new Date().toString()}`)
